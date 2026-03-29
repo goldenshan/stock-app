78df386c496e0557c4999ff57a967e5f35f37520
goldenshan <159632584+goldenshan@users.noreply.github.com>
2026-03-29 19:55:34 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import MSALProvider from "./providers/MSALProvider";
 
 export const metadata: Metadata = {
   title: "StockApp",
   description: "ระบบตรวจนับสต๊อค / ยืม-คืนอุปกรณ์",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="th">
-      <body>{children}</body>
+      <body>
+        <MSALProvider>{children}</MSALProvider>
+      </body>
     </html>
   );
 }
